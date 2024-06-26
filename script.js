203c46dabd09dedd20a1e5e27267221d140b7d46
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-26 11:52:57 +0530

    Script with no styling

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-let url = fetch('https://goweather.herokuapp.com/weather/india')
-console.log(url);
+let btn = document.querySelector('#submit');
+let showCity = document.querySelector('#cityShow');
+let dis = document.querySelector('#description');
+let temp = document.querySelector('#temp');
+let wind = document.querySelector('#wind');
+let forecastDiv = document.querySelector('#forecast');
+
+btn.addEventListener('click', weather);
+
+async function weather() {
+    let city = document.querySelector('#city').value;
+    console.log(city);
+
+    let response = await fetch(`https://goweather.herokuapp.com/weather/${city}`);
+    let data = await response.json();
+    showCity.innerHTML = city;
+    temp.innerHTML = data.temperature;
+    wind.innerHTML = data.wind;
+    dis.innerHTML = data.description;
+
+    forecastDiv.innerHTML = '';
+    
+
+}
