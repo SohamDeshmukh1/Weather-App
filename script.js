0fcb6ac0a9d9cb97030ebe2de7f4e00519afc859
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-26 12:09:08 +0530

    Working Js

--- script.js
+++ script.js
@@ -13,12 +13,31 @@ async function weather() {
 
     let response = await fetch(`https://goweather.herokuapp.com/weather/${city}`);
     let data = await response.json();
+    console.log(data);
     showCity.innerHTML = city;
     temp.innerHTML = data.temperature;
     wind.innerHTML = data.wind;
     dis.innerHTML = data.description;
 
     forecastDiv.innerHTML = '';
-    
+    // await data.forecast.foreach(
+    //     (day) => {  
+    //         let daydivs = document.createElement('div');
+    //         let daydivs = document.createElement('div');
+    //         daydivs.innerHTML = `<h2>Day:${day.temperature}</h2>`;
+    //         daydivs.innerHTML = `<h2>Day:${day.wind}</h2>`;
+    //     }
+    // );
 
-}
+    for (let i of data.forecast){
+        let daydivs = document.createElement('div');
+        daydivs.innerHTML = `<h2>Day:${i.day}</h2><h3>
+        <h2>Temp:${i.temperature}</h2>
+        <h2>Wind:${i.wind}</h2>`;
+        forecastDiv.appendChild(daydivs);
+        
+    }
+    
+    // forecastDiv.appendChild(forecast);
+
+    }
